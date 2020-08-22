ae361c7a716f8357ad70a3032e1b960549d039ee
Tim Aadland <potetstapp@gmail.com>
2020-08-22 14:44:20 +0200

    Renewed the program from a unfair game into: computer chooses a random option and compare it to the users option to determine who is the winner. The output line is depending on the result of the game: Win, Lose or Draw.

--- janken_pon.py
+++ janken_pon.py
@@ -1,19 +1,19 @@
 '''
-Program so far: An unfair version.
+Program so far: lose, draw and win provided from the computer and the user input.
 1. Take an input from the user
-2. Find an option that wins over the user's option
-3. Output a line : Sorry, but the computer chose {option}
+2. computer chooses a random option
+3. Compare the option from the computer and the user to determine the possible winner
+4. Out put status depending on the game: Win, Lose or Draw
 '''
-
-
-scissors = "scissors"
-rock = "rock"
-paper = "paper"
+import random
+print("Welcome to the Janken pon game: please choose between (rock, paper or scissors): ")
+computer = random.choice(['rock', 'paper', 'scissors'])
+user_winner = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
 user = input()
 
-if user == "scissors":
-    print(f"Sorry, but the computer chose {rock}")
-elif user == "rock":
-    print(f"Sorry, but the computer chose {paper}")
-elif user == "paper":
-    print(f"Sorry, but the computer chose {scissors}")
+if user == computer:
+    print(f"There is a draw ({computer})")
+elif user_winner[user] == computer:
+    print(f"Well done. The computer chose {computer} and failed")
+else:
+    print(f"Sorry, but the computer chose {computer}")
