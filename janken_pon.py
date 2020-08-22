317f54fd2ecd789c25e058516dcca1b33f11e897
Tim Aadland <potetstapp@gmail.com>
2020-08-22 15:01:05 +0200

    Refactored the program into a class. Making it easier to overwiew each methods.

--- janken_pon.py
+++ janken_pon.py
@@ -6,14 +6,38 @@ Program so far: lose, draw and win provided from the computer and the user input
 4. Out put status depending on the game: Win, Lose or Draw
 '''
 import random
-print("Welcome to the Janken pon game: please choose between (rock, paper or scissors): ")
-computer = random.choice(['rock', 'paper', 'scissors'])
-user_winner = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
-user = input()
 
-if user == computer:
-    print(f"There is a draw ({computer})")
-elif user_winner[user] == computer:
-    print(f"Well done. The computer chose {computer} and failed")
-else:
-    print(f"Sorry, but the computer chose {computer}")
+class JankenPon:
+    def __init__(self, user_option):
+        self.user_option = user_option
+        self.computer = random.choice(['rock', 'paper', 'scissors'])
+        self.user_winner = {'rock': 'scissors',
+                            'paper': 'rock',
+                            'scissors': 'paper'}
+
+    def win(self):
+        print(f"Well done. The computer chose {self.computer} and failed")
+
+    def lose(self):
+        print(f"Sorry, but the computer chose {self.computer}")
+
+    def draw(self):
+        print(f"There is a draw ({self.computer})")
+
+    def game_state(self):
+        if self.user_option == self.computer:
+            self.draw()
+        elif self.user_winner[self.user_option] == self.computer:
+            self.win()
+        else:
+            self.lose()
+    def game_intro(self):
+        print("Welcome to the Janken pon game: please choose between (rock, paper or scissors): ")
+
+
+game_start = ""
+JankenPon.game_intro(game_start)
+
+user_input = input()
+user = JankenPon(user_input)
+user.game_state()
